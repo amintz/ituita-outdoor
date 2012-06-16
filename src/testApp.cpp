7cfb0a3428df1cc04bddc516c32dfea39346317d
Andre Mintz <andre@nowhere>
2012-06-16 00:36:19 -0300

    Working Kinect Point Cloud Visualizing. Imported features from ofxKinect example.

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -2,21 +2,149 @@
 
 //--------------------------------------------------------------
 void testApp::setup(){
-
+    ofSetLogLevel(OF_LOG_VERBOSE);
+	
+	// enable depth->video image calibration
+	kinect.setRegistration(true);
+    
+	//kinect.init();
+	//kinect.init(true); // shows infrared instead of RGB video image
+	kinect.init(false, false); // disable video image (faster fps)
+	kinect.open();
+	
+#ifdef USE_TWO_KINECTS
+	kinect2.init();
+	kinect2.open();
+#endif
+    
+    // zero the tilt on startup
+	angle = 0;
+	kinect.setCameraTiltAngle(angle);
+    
+    
 }
 
 //--------------------------------------------------------------
 void testApp::update(){
+    
+    ofBackground(100, 100, 100);
+	
+	kinect.update();
+    
+#ifdef USE_TWO_KINECTS
+	kinect2.update();
+#endif
 
 }
 
 //--------------------------------------------------------------
 void testApp::draw(){
+    
+    ofSetColor(255, 255, 255);
+	
+    easyCam.begin();
+    drawPointCloud();
+    easyCam.end();
 
+}
+
+void testApp::drawPointCloud() {
+	int w = 640;
+	int h = 480;
+	ofMesh mesh;
+	mesh.setMode(OF_PRIMITIVE_POINTS);
+	int step = 2;
+	for(int y = 0; y < h; y += step) {
+		for(int x = 0; x < w; x += step) {
+			if(kinect.getDistanceAt(x, y) > 0) {
+				mesh.addColor(0xFFFFFF);
+				mesh.addVertex(kinect.getWorldCoordinateAt(x, y));
+			}
+		}
+	}
+	glPointSize(3);
+	ofPushMatrix();
+	// the projected points are 'upside down' and 'backwards' 
+	ofScale(1, -1, -1);
+	ofTranslate(0, 0, -1000); // center the points a bit
+	glEnable(GL_DEPTH_TEST);
+	mesh.drawVertices();
+	glDisable(GL_DEPTH_TEST);
+	ofPopMatrix();
+}
+
+//--------------------------------------------------------------
+void testApp::exit() {
+	kinect.setCameraTiltAngle(0); // zero the tilt on exit
+	kinect.close();
+	
+#ifdef USE_TWO_KINECTS
+	kinect2.close();
+#endif
 }
 
 //--------------------------------------------------------------
 void testApp::keyPressed(int key){
+    
+    switch (key) {
+//		case ' ':
+//			bThreshWithOpenCV = !bThreshWithOpenCV;
+//			break;
+//			
+//		case'p':
+//			bDrawPointCloud = !bDrawPointCloud;
+//			break;
+//			
+//		case '>':
+//		case '.':
+//			farThreshold ++;
+//			if (farThreshold > 255) farThreshold = 255;
+//			break;
+//			
+//		case '<':
+//		case ',':
+//			farThreshold --;
+//			if (farThreshold < 0) farThreshold = 0;
+//			break;
+//			
+//		case '+':
+//		case '=':
+//			nearThreshold ++;
+//			if (nearThreshold > 255) nearThreshold = 255;
+//			break;
+//			
+//		case '-':
+//			nearThreshold --;
+//			if (nearThreshold < 0) nearThreshold = 0;
+//			break;
+			
+		case 'w':
+			kinect.enableDepthNearValueWhite(!kinect.isDepthNearValueWhite());
+			break;
+			
+		case 'o':
+			kinect.setCameraTiltAngle(angle); // go back to prev tilt
+			kinect.open();
+			break;
+			
+		case 'c':
+			kinect.setCameraTiltAngle(0); // zero the tilt
+			kinect.close();
+			break;
+			
+		case OF_KEY_UP:
+			angle++;
+			if(angle>30) angle=30;
+			kinect.setCameraTiltAngle(angle);
+			break;
+			
+		case OF_KEY_DOWN:
+			angle--;
+			if(angle<-30) angle=-30;
+			kinect.setCameraTiltAngle(angle);
+			break;
+	}
+
 
 }
 
--- src/testApp.h
+++ src/testApp.h
@@ -1,12 +1,20 @@
 #pragma once
 
 #include "ofMain.h"
+//#include "ofxOpenCv.h"
+#include "ofxKinect.h"
+
+// uncomment this to read from two kinects simultaneously
+//#define USE_TWO_KINECTS
 
 class testApp : public ofBaseApp{
 	public:
 		void setup();
 		void update();
 		void draw();
+        void exit();
+    
+        void drawPointCloud();
 		
 		void keyPressed(int key);
 		void keyReleased(int key);
@@ -17,4 +25,16 @@ class testApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
+    
+    ofxKinect kinect;
+	
+#ifdef USE_TWO_KINECTS
+	ofxKinect kinect2;
+#endif
+    
+    // used for viewing the point cloud
+	ofEasyCam easyCam;
+    
+    int angle;
+    
 };
