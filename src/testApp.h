e0ce1566e6ef1f23f5556a882859c01c8d20775e
Felipe Turcheti <fturcheti@gmail.com>
2012-08-22 20:12:44 -0300

    removed ituitaBlobTracker object from testApp.h
    the blob tracking is encapsulated in kinectCapture

--- src/testApp.h
+++ src/testApp.h
@@ -123,9 +123,6 @@ class testApp : public ofBaseApp{
 // MARK: KINECT AND RELATED OBJECTS DECLARATION
     
         kinectCapture kinect;
-
-        //ituitaBlobTracker blobTracker;   
-
     
 // --------------------------------------------
 // MARK: INTERFACE VARIABLES
